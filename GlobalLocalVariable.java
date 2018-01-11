186344c9e98790a50d7071ab9d18e6d25d7e1945
Sovan Panda <35234742+SovanCSE@users.noreply.github.com>
2018-01-11 20:06:59 +0530

    Update GlobalLocalVariable.java

--- GlobalLocalVariable.java
+++ GlobalLocalVariable.java
@@ -1,3 +1,5 @@
+
+Program Example 1:
 class A {
 
 	// Global variable declared out side the
@@ -33,3 +35,56 @@ public class LocalClass {
 	}
 
 }
+
+Output:
+100 
+100	
+
+
+
+Program Example 2:
+class A {
+
+	// Global variable declared out side the
+	// method but inside the class is a
+	// global variable.
+
+	// Here data is Instance or global variable.
+	int data = 200;
+
+	// Local variable declare
+	// inside the method only.
+
+	// So variable data inside the
+	// method is local variable.
+	public A(int data) {
+		
+		//Here this keyword is 
+		//referring the global variable.
+		this.data = data;
+		
+		System.out.println(data);
+
+	}
+
+	void display() {
+		System.out.println(data);
+	}
+}
+
+public class LocalClass {
+
+	public static void main(String[] args) {
+
+		A obj = new A(100);
+		obj.display();
+
+	}
+
+}
+
+
+Output:
+
+100
+100
